19b1a57d0e8b52e62cefbe9da34c938b7c2054be
Henrik Norman <henrik@winterway.eu>
2019-08-23 17:38:03 +0000

    Removed unused publication classes
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3494 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/event/PublicationsClickEventHandler.java
+++
@@ -1,9 +0,0 @@
-package esac.archive.esasky.cl.web.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface PublicationsClickEventHandler extends EventHandler {
-
-    void onClick(PublicationsClickEvent event);
-
-}
