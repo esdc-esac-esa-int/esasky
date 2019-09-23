09875aa8100e49e2ad20b9d05dbbbff0659cc78d
Henrik Norman <henrik@winterway.eu>
2019-09-23 09:43:03 +0000

    ESASKYPCR-1546 - ESASky Switch can now also be used as a switch between two labels, not just as an on/off switch
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3554 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/common/EsaSkySwitch.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/common/EsaSkySwitch.java
@@ -2,6 +2,7 @@ package esac.archive.esasky.cl.web.client.view.common;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -21,20 +22,35 @@ public class EsaSkySwitch extends FocusPanel{
         CssResource style();
     }
 	
-	public EsaSkySwitch(String switchId, boolean isChecked, String label, String tooltip){
+    public EsaSkySwitch(String switchId, boolean isChecked, String label, String tooltip){
+    	this(switchId, isChecked, label, tooltip, null);
+    }
+    
+	public EsaSkySwitch(String switchId, boolean isChecked, String labelOption1, String tooltip, String labelOption2){
 		super();
 		this.resources = GWT.create(Resources.class);
 		this.style = this.resources.style();
 		this.style.ensureInjected();
 		this.switchId = switchId;
-		createSwitch(isChecked, "esasky__switch", label, tooltip);
+		createSwitch(isChecked, "esasky__switch", labelOption1, tooltip, labelOption2);
 	}
 
 	public void setChecked(boolean checked) {
 		Document.get().getElementById(switchId).setPropertyBoolean("checked", checked);
+		Element option1 = Document.get().getElementById(switchId + "__option1");
+		Element option2 = Document.get().getElementById(switchId + "__option2");
+		if(option1 != null && option2 != null) {
+			if(checked) {
+				option1.getStyle().setProperty("fontWeight", "normal");
+				option2.getStyle().setProperty("fontWeight", "bold");
+			} else {
+				option1.getStyle().setProperty("fontWeight", "bold");
+				option2.getStyle().setProperty("fontWeight", "normal");
+			}
+		}
 	}
 	
-	private void createSwitch(boolean isChecked, String cssPrefix, String label, String tooltip) {
+	private void createSwitch(boolean isChecked, String cssPrefix, String label, String tooltip, String labelOption2) {
 		FlowPanel switchContainer = new FlowPanel();
 		switchContainer.addStyleName(cssPrefix + "__container");
 
@@ -42,6 +58,7 @@ public class EsaSkySwitch extends FocusPanel{
 				"<label class=\"" + cssPrefix + "__label unselectable\" for=\"" + switchId + "\"> " 
 						+ label + "</label>");
 		switchLabel.addStyleName(cssPrefix + "__label-container");
+		switchLabel.getElement().setId(switchId + "__option1");
 		switchContainer.add(switchLabel);
 		String inputHtml = "  <input type=\"checkbox\" id=\"" + switchId + "\">";
 		if(isChecked) {
@@ -54,6 +71,21 @@ public class EsaSkySwitch extends FocusPanel{
 				"</label>");
 		htmlSwitch.addStyleName(cssPrefix + "__switch-container");
 		switchContainer.add(htmlSwitch);
+
+		if(labelOption2 != null) {
+			HTML switchLabelOption2 = new HTML(
+					"<label class=\"" + cssPrefix + "__label unselectable\" for=\"" + switchId + "\"> " 
+							+ labelOption2 + "</label>");
+			switchLabelOption2.addStyleName(cssPrefix + "__label-container");
+			switchLabelOption2.getElement().setId(switchId + "__option2");
+			switchContainer.add(switchLabelOption2);
+			htmlSwitch.addStyleName("esasky__switch__twoLabels");
+			if(isChecked) {
+				switchLabelOption2.getElement().getStyle().setProperty("fontWeight", "bold");
+			} else {
+				switchLabel.getElement().getStyle().setProperty("fontWeight", "bold");
+			}
+		}
 
 		switchContainer.addStyleName(cssPrefix + "__clickable-area");
 		switchContainer.setTitle(tooltip);
