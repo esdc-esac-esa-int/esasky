4663514a859c3019bd6145a42c49ef827732d5be
Mattias Wångblad <mattias@winterway.eu>
2019-09-26 12:38:18 +0000

    Added possibility to change column width and removed text-wrapping
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3592 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/table/SimpleTable.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/table/SimpleTable.java
@@ -1,12 +1,21 @@
 package esac.archive.esasky.cl.web.client.view.resultspanel.table;
 
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.EventTarget;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.TableCellElement;
+import com.google.gwt.dom.client.TableSectionElement;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+
 import esac.archive.esasky.cl.web.client.CommonEventBus;
 import esac.archive.esasky.cl.web.client.event.DataPanelResizeEvent;
 import esac.archive.esasky.cl.web.client.event.DataPanelResizeEventHandler;
@@ -16,6 +25,8 @@ public class SimpleTable<T extends TableRow> extends ESASkyDataGrid<T> {
 	private Resources resources;
 	private CssResource style;
 	private com.google.gwt.dom.client.Style tableContentStyle;
+	
+	private final int MIN_COLUMN_SIZE = 20;
 	
     public interface Resources extends ClientBundle {
         @Source("datagrid.css")
@@ -72,4 +83,140 @@ public class SimpleTable<T extends TableRow> extends ESASkyDataGrid<T> {
     	}
     	setScrollbarHeight();
     }
+    
+    private TableCellElement changingColumn;
+    private boolean ignoreClick = false;
+    int offset = 0;
+    
+    @Override
+    protected void onBrowserEvent2(Event event) {
+
+    	boolean hasTriggered = false;
+        EventTarget eventTarget = event.getEventTarget();
+        if (!Element.is(eventTarget)) {
+          return;
+        }
+        final Element target = event.getEventTarget().cast();
+        Element cell = ensureCellIsTarget(target);
+        int xValue = event.getClientX();
+        int xEndCell = cell.getAbsoluteLeft() + cell.getClientWidth();
+        
+        String eventType = event.getType();
+//        Log.debug(eventType);
+        if (Math.abs(xValue - xEndCell) < 5){
+        	if(BrowserEvents.MOUSEDOWN.equals(eventType)) {
+        		hasTriggered = true;
+        		changingColumn = target.cast();     
+        		offset = xValue - xEndCell;
+        		DOM.setCapture(this.getElement());
+        		
+        	}else if (BrowserEvents.MOUSEOVER.equals(eventType)){
+        		cell.addClassName("dataGridChangeColumnsSize");
+        	}
+        	else if (BrowserEvents.MOUSEOUT.equals(eventType)){
+        		cell.removeClassName("dataGridChangeColumnsSize");
+        	}
+        	
+        }else if (BrowserEvents.MOUSEMOVE.equals(eventType)){
+        	int newWidth = xValue - changingColumn.getAbsoluteLeft() - offset;
+        	int oldWidth = changingColumn.getClientWidth();
+        	if(newWidth > MIN_COLUMN_SIZE) {
+        		int col = changingColumn.getCellIndex();
+        		doSetColumnWidth(col,Integer.toString(newWidth)+"px");
+	        	int change = newWidth - oldWidth - 1;
+	        	int oldTableWidth = getTableBodyElement().getClientWidth();
+	    		setTableWidth(oldTableWidth + change, Unit.PX);
+        	}
+    		hasTriggered = true;
+    		
+    	}else {
+    		cell.removeClassName("dataGridChangeColumnsSize");
+        }
+    	if (BrowserEvents.MOUSEUP.equals(eventType)){
+    		DOM.releaseCapture(this.getElement());
+    		hasTriggered = true;
+    		ignoreClick = true;
+    		
+    	}else if (BrowserEvents.CLICK.equals(eventType) && ignoreClick){
+    		ignoreClick = false;
+    		hasTriggered = true;
+    	}
+        
+        if(!hasTriggered) {
+            Log.debug("Super event " + eventType);
+        	super.onBrowserEvent2(event);
+        }
+    }
+
+    //Mainly copied from AbstractCellTable
+    private Element ensureCellIsTarget(Element target) {
+    	Element maybeTableCell = null;
+        Element cur = target;
+        TableSectionElement tbody = getTableBodyElement();
+        TableSectionElement tfoot = getTableFootElement();
+        TableSectionElement thead = getTableHeadElement();
+        TableCellElement targetTableCell = null;
+
+        /*
+         * If an event happens in the TD element but outside the cell's div, we want 
+         * to handle it as if it happened within the table cell.
+         */
+        if (TableCellElement.TAG_TD.equalsIgnoreCase(cur.getTagName())) {
+          cur = cur.getFirstChildElement();
+        }
+        
+        while (cur != null) {
+          /*
+           * Found the table section. Return the most recent cell element that we
+           * discovered.
+           */
+          if (cur == tbody || cur == tfoot || cur == thead) {
+            if (maybeTableCell != null) {
+              targetTableCell = maybeTableCell.cast();
+              break;
+            }
+          }
+          
+          String tagName = cur.getTagName();
+          if (TableCellElement.TAG_TD.equalsIgnoreCase(tagName)
+              || TableCellElement.TAG_TH.equalsIgnoreCase(tagName)) {
+            /*
+             * Found a table cell, but we can't return yet because it may be part
+             * of a sub table within the a CellTable cell.
+             */
+            maybeTableCell = cur;
+          }
+          // Iterate.
+          cur = cur.getParentElement();
+        }
+        
+        return targetTableCell;
+    }
+    
+    // Default setWidth function doesn't work. 
+    // Sort event sets the the width of this element which causes multiple scrollers to appear
+    // Hopefully this is stable for all browsers
+    @Override
+    public void setTableWidth(double value, Unit unit) {
+    	getTableHeadElement().getParentElement().getParentElement().getParentElement().getParentElement().getStyle().setWidth(value, unit);
+      }
+    
+    public Element getHorizontalScrollableElement() {
+    	Element currElement = getTableHeadElement();
+    	Element oldElement = currElement;
+    	while(true) {
+    		if(currElement != null) {
+    			oldElement = currElement;
+    			currElement = currElement.getParentElement();
+    			if(currElement.hasClassName("dataPanelHoverDetector")) {
+    				break;
+    			}
+    		}else {
+    			currElement = oldElement;
+    			break;
+    		}
+    	}
+    	return currElement;
+    }
+    
 }
