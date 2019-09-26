965c77bc95fdde4a467f1f219ba76b66d17784e6
Mattias Wångblad <mattias@winterway.eu>
2019-09-26 13:47:50 +0000

    Enabled intersect search for CADC TAP
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3595 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/query/TAPExtTapService.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/query/TAPExtTapService.java
@@ -2,11 +2,13 @@ package esac.archive.esasky.cl.web.client.query;
 
 import com.allen_sauer.gwt.log.client.Log;
 
+import esac.archive.esasky.ifcs.model.coordinatesutils.CoordinatesConversion;
 import esac.archive.esasky.ifcs.model.coordinatesutils.SkyViewPosition;
 import esac.archive.esasky.ifcs.model.descriptor.ExtTapDescriptor;
 import esac.archive.esasky.ifcs.model.descriptor.IDescriptor;
 import esac.archive.esasky.cl.web.client.utility.AladinLiteWrapper;
 import esac.archive.esasky.cl.web.client.utility.CoordinateUtils;
+import esac.archive.esasky.cl.web.client.utility.EsaSkyWebConstants;
 
 public class TAPExtTapService extends AbstractMetadataService {
 
@@ -28,7 +30,11 @@ public class TAPExtTapService extends AbstractMetadataService {
          String parsedAdql = adql;
          parsedAdql += " from " + descriptor.getTapTable() + " WHERE ";
 
-         parsedAdql += raDecCenterSearch(descriptor);
+         if(descriptor.getSearchFunction().equals("polygonIntersect")) {
+        	 parsedAdql += polygonIntersectSearch(descriptor);
+         }else {
+        	 parsedAdql += raDecCenterSearch(descriptor);
+         }
          
          if(descriptor.getWhereADQL() != null) {
          	parsedAdql += " AND " + descriptor.getWhereADQL();
@@ -38,14 +44,45 @@ public class TAPExtTapService extends AbstractMetadataService {
          return parsedAdql;
     }
     
+    private String polygonIntersectSearch(ExtTapDescriptor descriptor) {
+    	String constraint = "1=INTERSECTS(" + descriptor.getTapSTCSColumn() + ",";
+        String shape = null;
+        double fovDeg = AladinLiteWrapper.getAladinLite().getFovDeg();
+        if (AladinLiteWrapper.isCornersInsideHips()) {
+            if (fovDeg < 1) {
+                shape = "POLYGON('ICRS', "
+                        + AladinLiteWrapper.getAladinLite().getFovCorners(1).toString() + ")";
+
+            } else {
+                shape = "POLYGON('ICRS', "
+                        + AladinLiteWrapper.getAladinLite().getFovCorners(2).toString() + ")";
+            }
+        } else {
+
+            String cooFrame = AladinLiteWrapper.getAladinLite().getCooFrame();
+            if (EsaSkyWebConstants.ALADIN_GALACTIC_COOFRAME.equalsIgnoreCase(cooFrame)) {
+                // convert to J2000
+                Double[] ccInJ2000 = CoordinatesConversion.convertPointGalacticToJ2000(
+                        AladinLiteWrapper.getAladinLite().getCenterLongitudeDeg(),
+                        AladinLiteWrapper.getAladinLite().getCenterLatitudeDeg());
+                shape = "CIRCLE('ICRS', " + ccInJ2000[0] + "," + ccInJ2000[1] + ",90)";
+            } else {
+                shape = "CIRCLE('ICRS', "
+                        + AladinLiteWrapper.getAladinLite().getCenterLongitudeDeg() + ","
+                        + AladinLiteWrapper.getAladinLite().getCenterLatitudeDeg() + ",90)";
+            }
+
+        }
+        return constraint + shape + ")";
+    }
+    
     private String raDecCenterSearch(ExtTapDescriptor descriptor) {
-        String[] fovCorners = AladinLiteWrapper.getAladinLite().getFovCorners(2).toString().split(",");
         double minRa = 999.0;
         double maxRa = -999.0;
         double minDec = 999.0;
         double maxDec = -999.0;
 
-        if(fovCorners.length < 16) {
+        if(!AladinLiteWrapper.isCornersInsideHips()) {
         	SkyViewPosition pos = CoordinateUtils.getCenterCoordinateInJ2000();
         	minRa = pos.getCoordinate().ra - pos.getFov()/2;
         	maxRa = pos.getCoordinate().ra + pos.getFov()/2;
@@ -53,7 +90,7 @@ public class TAPExtTapService extends AbstractMetadataService {
         	maxDec = pos.getCoordinate().dec + pos.getFov()/2;
         	
         }else {
-	        
+        	String[] fovCorners = AladinLiteWrapper.getAladinLite().getFovCorners(2).toString().split(",");
 	        for(int i = 0; i < fovCorners.length - 1; i += 2) {
 	        	double ra = Double.parseDouble(fovCorners[i]);
 	        	double dec = Double.parseDouble(fovCorners[i+1]);
