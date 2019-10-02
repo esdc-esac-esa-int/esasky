552db6c4b9d303c7d1da99d01cbe90682d885013
Henrik Norman <henrik@winterway.eu>
2019-10-02 05:27:59 +0000

    Now catching, logging, and notifying GA about any exception thrown during the processing of an http response
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3619 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/callback/JsonRequestCallback.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/callback/JsonRequestCallback.java
@@ -10,20 +10,29 @@ import com.google.gwt.http.client.Response;
 import esac.archive.esasky.cl.web.client.CommonEventBus;
 import esac.archive.esasky.cl.web.client.event.ProgressIndicatorPopEvent;
 import esac.archive.esasky.cl.web.client.event.ProgressIndicatorPushEvent;
+import esac.archive.esasky.cl.web.client.utility.GoogleAnalytics;
 
 public abstract class JsonRequestCallback implements RequestCallback {
 
     private String progressIndicatorId;
+    private String googleAnalyticsErrorMessage = "";
+    private String responseText = "";
     
     public JsonRequestCallback(String progressIndicatorMessage, String googleAnalyticsErrorMessage) {
         this.progressIndicatorId = UUID.randomUUID().toString();
+        this.googleAnalyticsErrorMessage = googleAnalyticsErrorMessage;
         addProgressIndicator(progressIndicatorMessage, googleAnalyticsErrorMessage);
     }
 
     @Override
     public void onResponseReceived(final Request request, final Response response) {
+    	this.responseText = response.getText();
         if (200 == response.getStatusCode()) {
-			onSuccess(response);
+        	try {
+        		onSuccess(response);
+        	} catch (Exception e) {
+        		onError(request, e);
+        	}
         } else {
             onError(request, new Exception(response.getStatusCode() + " ("
                     + response.getStatusText() + ")"));
@@ -37,8 +46,8 @@ public abstract class JsonRequestCallback implements RequestCallback {
     @Override
     public void onError(final Request request, final Throwable exception) {
         Log.error(exception.getMessage());
-        Log.error(this.getClass().getSimpleName() + " Error fetching JSON data from server");
-        
+        Log.error(this.getClass().getSimpleName() + " Error fetching JSON data from server. " + " Response text = " + responseText + " GA error message = " + googleAnalyticsErrorMessage);
+        GoogleAnalytics.sendEvent(GoogleAnalytics.CAT_RequestError, this.getClass().getSimpleName(), exception.getMessage() + " Error fetching JSON data from server. " + " Response text = " + responseText + " GA error message = " + googleAnalyticsErrorMessage);
         removeProgressIndicator();
     }
 
