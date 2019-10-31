fd2dfdde889d3de3d822bf5f4815899e0d557571
Henrik Norman <henrik@winterway.eu>
2019-10-31 16:51:47 +0000

    ESASKYPCR-1607 - Added utility class to convert a byte number into a user friendly format
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3733 aa7853a0-da02-4334-bf23-07ffea9f4f01

---
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/SizeFormatter.java
@@ -0,0 +1,13 @@
+package esac.archive.esasky.cl.web.client.utility;
+
+public final class SizeFormatter {
+
+	public final native static String formatBytes(Long bytes, int decimals) /*-{ 
+	   if(bytes == 0) return '0 Bytes';
+	   var k = 1024, 
+	   dm = decimals <= 0 ? 0 : decimals || 2, 
+	   sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'], 
+	   i = Math.floor(Math.log(bytes) / Math.log(k));
+	   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+	}-*/;
+}
