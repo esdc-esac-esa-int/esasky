5faa964dc4f93f5ba2370d5b44bbb24f98f0f505
Mattias Wångblad <mattias@winterway.eu>
2019-11-01 14:17:08 +0000

    Fixed error on 1 color
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@3747 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/EsaSkyColors.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/EsaSkyColors.java
@@ -3,7 +3,7 @@ package esac.archive.esasky.cl.web.client.utility;
 
 public class EsaSkyColors {
 	public static String[] colors = new String[]{"#E8ECFB", "#D9CCE3", "#CAACCB", "#BA8DB2", "#AA6F9E", "#994F88", "#882E72", "#1965B0",
-			"#437DBF", "#6195CF", "#7BAFDE", "#4EB265", "#90C987", "#CAE0AB", "#F7F056", "#F7CB45", "#F4A736", "#EE8026", "#E665518",
+			"#437DBF", "#6195CF", "#7BAFDE", "#4EB265", "#90C987", "#CAE0AB", "#F7F056", "#F7CB45", "#F4A736", "#EE8026", "#E65518",
 			"#DC050C", "#A5170E", "#72190E", "#42150A"};
 	
 	private static Integer index;
