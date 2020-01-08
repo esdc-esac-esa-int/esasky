2c3587272bd8a5e53d573388b25bb9c47e97efad
Mattias Wångblad <mattias@winterway.eu>
2020-01-08 12:53:27 +0000

    Added fucntion for exporting row to API
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4020 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/AbstractTablePanel.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/AbstractTablePanel.java
@@ -1201,6 +1201,11 @@ public abstract class AbstractTablePanel extends Composite {
 		}
 	}
 	
+	public String getUnfilteredRow(int rowIndex) {
+		TapRowList rowList = getEntity().getMetadata();
+		return rowList.getData().get(rowIndex).toString(); 
+	}
+	
 	public JSONObject exportAsJSON() {
 		JSONObject jsonData = new JSONObject();
 		TapRowList rowList = getEntity().getMetadata();
