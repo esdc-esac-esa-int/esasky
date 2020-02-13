6ce88d56d9e80d6de0ff8603b2b2e3655573f756
Mattias Wångblad <mattias@winterway.eu>
2020-02-13 09:09:44 +0000

    Added filter listeners to all columns
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4106 aa7853a0-da02-4334-bf23-07ffea9f4f01

---
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/AbstractTableFilterObserver.java
@@ -0,0 +1,7 @@
+package esac.archive.esasky.cl.web.client.view.resultspanel;
+
+import java.util.Map;
+
+public interface AbstractTableFilterObserver {
+	public void filterChanged(Map<String, String> tapFilters);
+}
