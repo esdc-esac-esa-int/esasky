0dc0901b1b79931d3d3374e3c5250fb3739858ea
Henrik Norman <henrik@winterway.eu>
2020-03-16 15:14:23 +0000

    Corrected GA event for closing welcome dialog
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4261 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/GoogleAnalytics.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/GoogleAnalytics.java
@@ -147,7 +147,7 @@ public final class GoogleAnalytics {
     
     public static final String ACT_Welcome_Science = "Science";
     public static final String ACT_Welcome_Explorer = "Explorer";
-    public static final String ACT_Welcome_Close = "Previous";
+    public static final String ACT_Welcome_Close = "CloseWithoutSelection";
     public static final String ACT_Welcome_DoNotShowAgain = "DoNotShowAgain";
     
     public static final String ACT_Slider_Moved = "Moved";
