a217ed068ca17ad38b15cee5a605c4a60c2f987c
Henrik Norman <henrik@winterway.eu>
2020-04-02 10:20:26 +0000

    Removed unused variable & imports
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4333 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/callback/MetadataCallback.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/callback/MetadataCallback.java
@@ -1,19 +1,13 @@
 package esac.archive.esasky.cl.web.client.callback;
 
 import java.util.HashMap;
-import java.util.List;
-
 import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.http.client.Response;
 
-import esac.archive.esasky.cl.web.client.model.TableRow;
 import esac.archive.esasky.cl.web.client.model.TapRowList;
-import esac.archive.esasky.cl.web.client.model.converter.TapToMmiDataConverter;
-import esac.archive.esasky.cl.web.client.model.entities.EntityContext;
-import esac.archive.esasky.cl.web.client.model.entities.ExtTapEntity;
 import esac.archive.esasky.cl.web.client.model.entities.GeneralEntityInterface;
 import esac.archive.esasky.cl.web.client.presenter.ResultsPresenter.TapRowListMapper;
 import esac.archive.esasky.cl.web.client.view.resultspanel.ITablePanel;
@@ -22,7 +16,6 @@ public class MetadataCallback extends JsonRequestCallback {
 
 	private GeneralEntityInterface entity;
 	private ITablePanel tablePanel;
-	private String adql;
 	private static HashMap<String, Long> latestUpdates = new HashMap<String, Long>();
 	private long timecall;
 	private OnComplete onComplete;
@@ -43,7 +36,6 @@ public class MetadataCallback extends JsonRequestCallback {
 		super(progressIndicatorMessage, adql);
 		this.entity = tablePanel.getEntity();
 		this.tablePanel = tablePanel;
-		this.adql = adql;
 		timecall = System.currentTimeMillis();
 		latestUpdates.put(tablePanel.getEsaSkyUniqID(), timecall);
 		startTime = System.currentTimeMillis();
