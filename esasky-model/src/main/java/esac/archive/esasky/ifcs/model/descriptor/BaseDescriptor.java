35ff89cf11fbffde6be5caeed9b9e41e217ff171
Mattias Wångblad <mattias@winterway.eu>
2020-05-04 07:48:10 +0000

    Fixed ignore for metadata to json function
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4387 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-model/src/main/java/esac/archive/esasky/ifcs/model/descriptor/BaseDescriptor.java
+++ esasky-model/src/main/java/esac/archive/esasky/ifcs/model/descriptor/BaseDescriptor.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.allen_sauer.gwt.log.client.Log;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -479,13 +480,14 @@ public abstract class BaseDescriptor implements IDescriptor {
         this.shapeLimit = shapeLimit;
     }
     
+    @JsonIgnore
     public GeneralJavaScriptObject getMetaDataJSONObject() {
     	
     	GeneralJavaScriptObject object = GeneralJavaScriptObject.createJsonObject("{}");
     	int i = 0;
     	for(MetadataDescriptor md : getMetadata()) {
     		GeneralJavaScriptObject mdObject = md.toJSONObject();
-    		mdObject.setProperty("index", i);
+    		mdObject.setProperty("index", Integer.toString(i));
     		object.setProperty(md.getTapName(), mdObject);
     		i++;
     	}
