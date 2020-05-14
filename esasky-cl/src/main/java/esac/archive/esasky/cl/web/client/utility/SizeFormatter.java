3aebd046e4c765a1ce9a032c2f04c6b62fcad51f
Henrik Norman <henrik@winterway.eu>
2020-05-14 07:18:13 +0000

    ESASKYPCR-1648 - file sizeFormatter can now parse strings to bytes as well as bytes to strings
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4452 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/SizeFormatter.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/SizeFormatter.java
@@ -3,11 +3,33 @@ package esac.archive.esasky.cl.web.client.utility;
 public final class SizeFormatter {
 
 	public final native static String formatBytes(int bytes, int decimals) /*-{ 
-	   if(bytes == 0) return '0 Bytes';
-	   var k = 1024, 
-	   dm = decimals <= 0 ? 0 : decimals || 2, 
-	   sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'], 
-	   i = Math.floor(Math.log(bytes) / Math.log(k));
-	   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+        if(bytes == 0) return '0 Bytes';
+        var k = 1024, 
+        dm = decimals <= 0 ? 0 : decimals || 2, 
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'], 
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+	}-*/;
+	
+	public final native static double formatToBytes(String fileSize) /*-{ 
+	    var numberString = "";
+	    
+        var split = fileSize.split(/[A-Za-z]/);
+        for(var i = 0; i < split.length; i++) {
+            if(!isNaN(split[i]) && split[i].trim().length > 0){
+                numberString = split[i];
+                break;
+            }
+        }
+	    
+        var sizeSuffix = fileSize.split(numberString)[1].match(/[A-Za-z]+/);
+        var i = 0;
+        var sizeSuffixPossibilities = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        sizeSuffixPossibilities.forEach(function (suffix){
+            if(sizeSuffix && sizeSuffix[0] && sizeSuffix[0].toLowerCase().includes(suffix.toLowerCase())){
+                i = sizeSuffixPossibilities.indexOf(suffix);
+            }
+        });
+        return parseFloat(numberString) * Math.pow(1024, i);
 	}-*/;
 }
