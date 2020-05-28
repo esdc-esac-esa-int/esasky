605be5b20ce5cb1d630567d19dbc32d09d65ceb6
Mattias Wångblad <mattias@winterway.eu>
2020-05-28 11:24:53 +0000

    Make sure to clear all filters on refresh
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4499 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/TabulatorWrapper.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/resultspanel/TabulatorWrapper.java
@@ -264,6 +264,7 @@ public class TabulatorWrapper{
     	tableJsObject.clearData();
     	tableJsObject.filterData = [];
     	tableJsObject.metadata = [];
+    	tableJsObject.clearHeaderFilter();
     	previouslySelectedMap = [];
     }-*/;
 
