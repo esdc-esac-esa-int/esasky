0a5d263aa2fbccde3becfddd5c66aa2cc6af9571
Henrik Norman <henrik@winterway.eu>
2020-05-29 08:33:21 +0000

    ESASKYPCR-1738 - console logs now only active if log_level=DEBUG url parameter is set
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@4508 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/webapp/js/esasky-logger.js
+++ esasky-cl/src/main/webapp/js/esasky-logger.js
@@ -2,41 +2,49 @@ var startupTime = Date.now();
 console.defaultLog = console.log.bind(console);
 console.logs = [];
 console.log = function(){
-    console.defaultLog.apply(console, arguments);
+	if(window.location.href.includes("log_level=DEBUG")){
+    	console.defaultLog.apply(console, arguments);
+	}
     console.logs.push(Array.from(arguments));
 }
 console.defaultError = console.error.bind(console);
 console.errors = [];
 console.error = function(){
-    console.defaultError.apply(console, arguments);
+	if(window.location.href.includes("log_level=DEBUG")){
+	    console.defaultError.apply(console, arguments);
+	}	
     console.errors.push(Array.from(arguments));
     try{
-    var lastDebugs = [];
-    if(console.debugs.length < 20){
-		lastDebugs = console.debugs;    
-    } else{
-    	for(var i = 20; i>0; i--){
-    		lastDebugs.push(console.debugs[console.debugs.length - i]);
-    	}
-    }
-    window.ga('send', 'event', "Error", "console.error", "Time since startup: " 
-    + (Date.now() - startupTime) + " (millis)"
-    + " |||| Logged Errors: " + console.errors + " |||| Logged Warnings: " + console.warns 
-    + " |||| Last Logged debugs: " + lastDebugs);
+	    var lastDebugs = [];
+	    if(console.debugs.length < 20){
+			lastDebugs = console.debugs;    
+	    } else {
+	    	for(var i = 20; i>0; i--){
+	    		lastDebugs.push(console.debugs[console.debugs.length - i]);
+	    	}
+	    }
+	    window.ga('send', 'event', "Error", "console.error", "Time since startup: " 
+	    	+ (Date.now() - startupTime) + " (millis)"
+	    	+ " |||| Logged Errors: " + console.errors + " |||| Logged Warnings: " + console.warns 
+	    	+ " |||| Last Logged debugs: " + lastDebugs);
     } catch(e){
     }
 }
 console.defaultWarn = console.warn.bind(console);
 console.warns = [];
 console.warn = function(){
-    console.defaultWarn.apply(console, arguments);
+	if(window.location.href.includes("log_level=DEBUG")){
+    	console.defaultWarn.apply(console, arguments);
+    }
     console.warns.push(Array.from(arguments));
 }
 
 console.defaultDebug = console.debug.bind(console);
 console.debugs = [];
 console.debug = function(){
-    console.defaultDebug.apply(console, arguments);
+	if(window.location.href.includes("log_level=DEBUG")){
+    	console.defaultDebug.apply(console, arguments);
+    }
     console.debugs.push(Array.from(arguments));
 }
 window.addEventListener('error', function(e) {
