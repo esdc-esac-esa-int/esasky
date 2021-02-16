8b0d87f17908e123268ac3eac3d3d4e06c15c07c
Mattias Wångblad <mattias@winterway.eu>
2021-02-16 16:53:08 +0000

    Fixed some more code smell
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@5324 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/Modules.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/Modules.java
@@ -6,6 +6,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.i18n.client.Dictionary;
 
 import esac.archive.esasky.cl.web.client.utility.EsaSkyWebConstants;
+import esac.archive.esasky.cl.web.client.utility.exceptions.MapKeyException;
 
 public class Modules {
     
@@ -17,7 +18,12 @@ public class Modules {
     public static boolean publicationsModule = true;
     public static boolean wwtLink = true;
     public static boolean toggleColumns = true; // set from Controller with toggle_column URL flag
-    public static String mode = "";
+    private static String mode = "";
+    private static HashMap<String, Boolean> moduleMap = new HashMap<>();
+    
+    private Modules() {
+    	
+    }
     
     public static final boolean shouldShowExtTap = Boolean.parseBoolean(Dictionary.getDictionary("serverProperties")
 			.get("showExtTap"));
@@ -43,73 +49,153 @@ public class Modules {
         }
     }
     
-    private static HashMap<String, Boolean> modules = new HashMap<>();
+
     
     public static boolean getModule(String key) {
-    	if(modules.containsKey(key)) {	
-    		return modules.get(key);
+    	if(moduleMap.containsKey(key)) {	
+    		return moduleMap.get(key);
     	}else {
     		return false;
     	}
     }
 
-    public static void setModule(String key, boolean value) throws Exception {
-    	if(modules.containsKey(key)) {
-    		modules.put(key, value);
+    public static void setModule(String key, boolean value) throws MapKeyException {
+    	if(moduleMap.containsKey(key)) {
+    		moduleMap.put(key, value);
     		return;
     	}
-    	throw new Exception(key);
+    	throw new MapKeyException(key);
     }
     
     public static String[] getModuleKeys(){
-    	return modules.keySet().toArray(new String [0]);
+    	return moduleMap.keySet().toArray(new String [0]);
 	}
     
     public static void setMode(String mode) {
     	Modules.mode = mode;
     	
     	if("JWST".equalsIgnoreCase(mode)) {
-    		modules.put(EsaSkyWebConstants.MODULE_SCIENTIFIC, true);
-    		modules.put(EsaSkyWebConstants.MODULE_LANGUAGE, false);
-    		modules.put(EsaSkyWebConstants.MODULE_COOR_GRID, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SCREENSHOT, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SHARE, true);
-    		modules.put(EsaSkyWebConstants.MODULE_HELP, false);
-    		modules.put(EsaSkyWebConstants.MODULE_DROPDOWN, false);
-    		modules.put(EsaSkyWebConstants.MODULE_FEEDBACK, false);
-    		modules.put(EsaSkyWebConstants.MODULE_SKIESMENU, true);
-    		modules.put(EsaSkyWebConstants.MODULE_OBS, true);
-    		modules.put(EsaSkyWebConstants.MODULE_CAT, false);
-    		modules.put(EsaSkyWebConstants.MODULE_SPE, false);
-    		modules.put(EsaSkyWebConstants.MODULE_EXTTAP, false);
-    		modules.put(EsaSkyWebConstants.MODULE_SSO, false);
-    		modules.put(EsaSkyWebConstants.MODULE_PUBLICATIONS, false);
-    		modules.put(EsaSkyWebConstants.MODULE_TARGETLIST, false);
-    		modules.put(EsaSkyWebConstants.MODULE_JWST_PLANNING, true);
-    		modules.put(EsaSkyWebConstants.MODULE_DICE, false);
-    		modules.put(EsaSkyWebConstants.MODULE_SCIENCE_MODE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SCIENTIFIC, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_LANGUAGE, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_COOR_GRID, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SCREENSHOT, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SHARE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_HELP, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_DROPDOWN, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_FEEDBACK, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SKIESMENU, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_OBS, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_CAT, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SPE, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_EXTTAP, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SSO, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_PUBLICATIONS, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_TARGETLIST, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_JWST_PLANNING, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_DICE, false);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SCIENCE_MODE, true);
     	}
     	else {
-    		modules.put(EsaSkyWebConstants.MODULE_SCIENTIFIC, true);
-    		modules.put(EsaSkyWebConstants.MODULE_LANGUAGE, true);
-    		modules.put(EsaSkyWebConstants.MODULE_COOR_GRID, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SCREENSHOT, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SHARE, true);
-    		modules.put(EsaSkyWebConstants.MODULE_HELP, true);
-    		modules.put(EsaSkyWebConstants.MODULE_DROPDOWN, true);
-    		modules.put(EsaSkyWebConstants.MODULE_FEEDBACK, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SKIESMENU, true);
-    		modules.put(EsaSkyWebConstants.MODULE_OBS, true);
-    		modules.put(EsaSkyWebConstants.MODULE_CAT, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SPE, true);
-    		modules.put(EsaSkyWebConstants.MODULE_EXTTAP, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SSO, true);
-    		modules.put(EsaSkyWebConstants.MODULE_PUBLICATIONS, true);
-    		modules.put(EsaSkyWebConstants.MODULE_TARGETLIST, true);
-    		modules.put(EsaSkyWebConstants.MODULE_JWST_PLANNING, true);
-    		modules.put(EsaSkyWebConstants.MODULE_DICE, true);
-    		modules.put(EsaSkyWebConstants.MODULE_SCIENCE_MODE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SCIENTIFIC, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_LANGUAGE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_COOR_GRID, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SCREENSHOT, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SHARE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_HELP, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_DROPDOWN, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_FEEDBACK, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SKIESMENU, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_OBS, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_CAT, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SPE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_EXTTAP, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SSO, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_PUBLICATIONS, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_TARGETLIST, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_JWST_PLANNING, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_DICE, true);
+    		moduleMap.put(EsaSkyWebConstants.MODULE_SCIENCE_MODE, true);
     	}
     }
+
+	public static boolean isProposalModule() {
+		return proposalModule;
+	}
+
+	public static boolean isBannersOnAllSides() {
+		return bannersOnAllSides;
+	}
+
+	public static boolean isInternationalization() {
+		return internationalization;
+	}
+
+	public static boolean isSsoModule() {
+		return ssoModule;
+	}
+
+	public static boolean isSpectraModule() {
+		return spectraModule;
+	}
+
+	public static boolean isPublicationsModule() {
+		return publicationsModule;
+	}
+
+	public static boolean isWwtLink() {
+		return wwtLink;
+	}
+
+	public static boolean isToggleColumns() {
+		return toggleColumns;
+	}
+
+	public static String getMode() {
+		return mode;
+	}
+
+	public static boolean isShouldshowexttap() {
+		return shouldShowExtTap;
+	}
+
+	public static HashMap<String, Boolean> getModuleMap() {
+		return moduleMap;
+	}
+
+	public static void setProposalModule(boolean proposalModule) {
+		Modules.proposalModule = proposalModule;
+	}
+
+	public static void setBannersOnAllSides(boolean bannersOnAllSides) {
+		Modules.bannersOnAllSides = bannersOnAllSides;
+	}
+
+	public static void setInternationalization(boolean internationalization) {
+		Modules.internationalization = internationalization;
+	}
+
+	public static void setSsoModule(boolean ssoModule) {
+		Modules.ssoModule = ssoModule;
+	}
+
+	public static void setSpectraModule(boolean spectraModule) {
+		Modules.spectraModule = spectraModule;
+	}
+
+	public static void setPublicationsModule(boolean publicationsModule) {
+		Modules.publicationsModule = publicationsModule;
+	}
+
+	public static void setWwtLink(boolean wwtLink) {
+		Modules.wwtLink = wwtLink;
+	}
+
+	public static void setToggleColumns(boolean toggleColumns) {
+		Modules.toggleColumns = toggleColumns;
+	}
+
+	public static void setModuleMap(HashMap<String, Boolean> moduleMap) {
+		Modules.moduleMap = moduleMap;
+	}
     
 }
---
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/exceptions/MapKeyException.java
@@ -0,0 +1,15 @@
+package esac.archive.esasky.cl.web.client.utility.exceptions;
+
+public class MapKeyException extends Exception {
+	private static final long serialVersionUID = -7837275152715500302L;
+	
+	public MapKeyException() {
+		super();
+	}
+
+	public MapKeyException(String errorMsg) {
+		super(errorMsg);
+	}
+	
+	
+}
