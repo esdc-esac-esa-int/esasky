118ef1b1e760bf0c8fb1b1493b95a1d51eed9e19
Mattias Wångblad <mattias@winterway.eu>
2021-05-03 06:45:33 +0000

    Added tests on the wavelengthColors
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@5453 aa7853a0-da02-4334-bf23-07ffea9f4f01

---
+++ esasky-model/src/test/esac/archive/esasky/ifcs/model/shared/EsaSkyColorsTest.java
@@ -0,0 +1,29 @@
+package esac.archive.esasky.ifcs.model.shared;
+
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import esac.archive.esasky.ifcs.model.shared.ESASkyColors;
+
+public class EsaSkyColorsTest {
+
+	
+	 @Test
+		    public void testWavelengthToIndex() {
+		 	assertEquals(ESASkyColors.wavelengthToIndex(0), 22);
+	        assertEquals(ESASkyColors.wavelengthToIndex(1.1), 21);
+	        assertEquals(ESASkyColors.wavelengthToIndex(-1.1), 21);
+	        assertEquals(ESASkyColors.wavelengthToIndex(6.5), 10);
+	        assertEquals(ESASkyColors.wavelengthToIndex(7), 8);
+	    }
+	 
+	 @Test
+	    public void testIndexToWavelength() {
+		 	assertThat(ESASkyColors.indexToWaveLength(11), is(closeTo(6.3, 0.01)));
+		 	assertThat(ESASkyColors.valueToWaveLength(11.5), is(closeTo(6.25, 0.01)));
+		 	assertThat(ESASkyColors.indexToWaveLength(16), is(closeTo(5, 0.1)));
+	    }
+}
