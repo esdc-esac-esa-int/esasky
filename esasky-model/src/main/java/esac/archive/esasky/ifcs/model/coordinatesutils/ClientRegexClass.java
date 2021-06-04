67e96db229552ad1f3dee69e9b007b83050c1d00
Mattias Wångblad <mattias@winterway.eu>
2021-06-04 08:40:48 +0000

    Added match to the regex class
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@5511 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-model/src/main/java/esac/archive/esasky/ifcs/model/coordinatesutils/ClientRegexClass.java
+++ esasky-model/src/main/java/esac/archive/esasky/ifcs/model/coordinatesutils/ClientRegexClass.java
@@ -1,5 +1,6 @@
 package esac.archive.esasky.ifcs.model.coordinatesutils;
 
+import esac.archive.esasky.ifcs.model.client.GeneralJavaScriptObject;
 import esac.archive.esasky.ifcs.model.coordinatesutils.CoordinateValidator.RegexClass;
 
 public class ClientRegexClass implements RegexClass{
@@ -10,6 +11,10 @@ public class ClientRegexClass implements RegexClass{
 	
     private native boolean regex(String pattern, String stringToTest)/*-{ 
 		return $wnd.XRegExp(pattern).test(stringToTest);
+	}-*/;
+
+    public native GeneralJavaScriptObject match(String pattern, String stringToTest)/*-{ 
+		return stringToTest.match(pattern);
 	}-*/;
 
 }
