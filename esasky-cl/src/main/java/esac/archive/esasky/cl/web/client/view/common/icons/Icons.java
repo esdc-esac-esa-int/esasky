e81c9e9901c384b0fc3633026b6a55220760bff8
Henrik Norman <henrik@winterway.eu>
2021-08-25 14:34:28 +0000

    Removed unused imports
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@5622 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/common/icons/Icons.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/common/icons/Icons.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.resources.client.ClientBundle.Source;
 import com.google.gwt.resources.client.ImageResource.ImageOptions;
 
 
