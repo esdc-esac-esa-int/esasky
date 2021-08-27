1f4c93644da6491e34e5c943ad8cdf9414761cf6
Mattias Wångblad <mattias@winterway.eu>
2021-08-27 13:17:38 +0000

    Removed all references to pyesasky in the cl project (Since cl is now not deployed inside pyesasky)
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@5651 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/pyesasky/pyesasky/nbextension/static/js/config.js
+++
@@ -1,8 +0,0 @@
-var serverProperties = {
- contextPath: "/esasky",
- tapContext: "http://sky.esa.int/esasky-tap",
- highchartsExportURL: "//highcharts.export.host/highcharts.export.context",
- localeFilesLocation: "",
- targetListFilesLocation: "",
- showMissingTranslationBox: "${show.missing.translation.box}"
-};
