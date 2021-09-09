7599352f8bada0821b0b814afa09fa8552136c39
Henrik Norman <henrik@winterway.eu>
2021-09-09 15:04:45 +0000

    Deleted moved files
    git-svn-id: https://satscm.esac.esa.int/esasky/esasky-app/trunk@5804 aa7853a0-da02-4334-bf23-07ffea9f4f01

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/ctrltoolbar/uploadtargetlist/TargetObserver.java
+++
@@ -1,5 +0,0 @@
-package esac.archive.esasky.cl.web.client.view.ctrltoolbar.uploadtargetlist;
-
-public interface TargetObserver {
-	public void onTargetSelectionEvent(TargetWidget newlySelectedTarget);
-}
