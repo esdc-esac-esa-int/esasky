d88c6cf8c82a13f4705a941f50516d73698fabba
mattiasw <mattias.wangblad@gmail.com>
2022-02-21 13:14:53 +0100

    Added save/restore session buttons to hamburger menu

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/EsaSkyWebConstants.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/utility/EsaSkyWebConstants.java
@@ -149,6 +149,7 @@ public class EsaSkyWebConstants {
     public static final String MODULE_JWST_PLANNING = "jwst_planning_button";
     public static final String MODULE_DICE = "dice_button";
     public static final String MODULE_SCIENCE_MODE = "science_mode";
+    public static final String MODULE_SESSION = "session";
     
     
     // DEFAULT ONES
