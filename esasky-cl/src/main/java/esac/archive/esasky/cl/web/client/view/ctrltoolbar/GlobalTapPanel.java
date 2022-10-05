b9505f1a0a846fda812e1c3b8c41252126610062
Philip Matsson <philip@winterway.eu>
2022-10-05 07:15:01 +0200

    ESASKYPCR-2345 - Disabled multiselect for TAP registry table

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/ctrltoolbar/GlobalTapPanel.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/view/ctrltoolbar/GlobalTapPanel.java
@@ -95,6 +95,7 @@ public class GlobalTapPanel extends MovableResizablePanel<GlobalTapPanel> {
         settings.setAddAdqlColumn(true);
         settings.setAddMetadataColumn(true);
         settings.setIsDownloadable(false);
+        settings.setSelectable(1);
         tabulatorTable = new TabulatorWrapper("browseTap__tabulatorContainer", new TabulatorCallback(), settings);
         tabulatorTable.groupByColumn("res_title");
         tabulatorTable.insertExternalTapData(obj.getProperty("data"), obj.getProperty("columns"));
