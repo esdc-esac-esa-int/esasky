f4bfc4b0a4393d6004a0e37da9348464dcf15853
Javier Espinosa <javier.espinosa@ext.esa.int>
2023-05-31 16:11:27 +0200

    ESASKYPCR-2413: all in sso schema

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/query/TAPSSOService.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/query/TAPSSOService.java
@@ -65,36 +65,36 @@ public class TAPSSOService extends AbstractTAPService {
         String dbSSOType = "aster";
         // TODO this must be changed! SSODnet and the ephemeris script uses different identifiers
 
-        String adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
+        String adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
                 + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
 
         if (ssoType == ESASkySSOObjType.ASTEROID) {
             dbSSOType = "aster";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         } else if (ssoType == ESASkySSOObjType.COMET) {
             dbSSOType = "comet";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_id = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_id = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         } else if (ssoType == ESASkySSOObjType.PLANET) {
             dbSSOType = "planet";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         } else if (ssoType == ESASkySSOObjType.SATELLITE) {
             dbSSOType = "satellite";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         } else if (ssoType == ESASkySSOObjType.SPACECRAFT) {
             dbSSOType = "spacecraft";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_id = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_id = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         } else if (ssoType == ESASkySSOObjType.DWARF_PLANET) {
             dbSSOType = "aster";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_name = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         } else if (ssoType == ESASkySSOObjType.SSO) {
             dbSSOType = "sso";
-            adql = "select b.* from sso_pipeline.ssoid as a join sso_pipeline.count_table as b on a.sso_oid = b.sso_oid where a.sso_id = '"
+            adql = "select b.* from sso.ssoid as a join sso.count_table as b on a.sso_oid = b.sso_oid where a.sso_id = '"
                     + ssoName + "' and a.sso_type = '" + dbSSOType + "'";
         }
 
