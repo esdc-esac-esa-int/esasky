e0cbbb0788ed266f50184ca2c8c6d3b5c950f507
Philip Matsson <philip@winterway.eu>
2024-09-04 15:19:33 +0200

    Fix improper loading of MOC data when MOCEntity is hidden

--- esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/model/entities/MOCEntity.java
+++ esasky-cl/src/main/java/esac/archive/esasky/cl/web/client/model/entities/MOCEntity.java
@@ -76,7 +76,7 @@ public class MOCEntity implements GeneralEntityInterface {
 			getVisibleCount();
 			setTableCountText();
 
-			if(filterRequested && perMissionNewCount < EsaSkyWebConstants.MOC_FILTER_LIMIT) {
+			if(filterRequested && perMissionNewCount < EsaSkyWebConstants.MOC_FILTER_LIMIT && shouldBeShown) {
 				loadMOC();
 				filterRequested = false;
 			}
@@ -92,15 +92,13 @@ public class MOCEntity implements GeneralEntityInterface {
 	
 		@Override
 		public void run() {
-			if (shouldBeShown) {
 				if (getCountStatus().hasMoved(descriptor)) {
 					filterRequested = true;
-				} else if( getCountStatus().getCount(descriptor) < EsaSkyWebConstants.MOC_FILTER_LIMIT){
+				} else if(getCountStatus().getCount(descriptor) < EsaSkyWebConstants.MOC_FILTER_LIMIT && shouldBeShown){
 					filterRequested = true;
 					loadMOC();
 					filterRequested = false;
 				}
-			}
 		}
 		
 		@Override
@@ -441,7 +439,7 @@ public class MOCEntity implements GeneralEntityInterface {
 		@Override
 		public void run() {
 
-				if (descriptor.hasSearchArea()) {
+				if (descriptor.hasSearchArea() || !shouldBeShown) {
 					return;
 				}
 
