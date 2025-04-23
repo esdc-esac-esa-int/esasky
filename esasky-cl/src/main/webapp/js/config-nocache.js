e2b950b6f9092ba6a12ca82c9292c7720dd73cb9
Henrik Norman <henrik@winterway.eu>
2025-04-23 14:56:56 +0200

    Added JWST time-series for local, dev, and int

--- esasky-cl/src/main/webapp/js/config-nocache.js
+++ esasky-cl/src/main/webapp/js/config-nocache.js
@@ -5,7 +5,8 @@ var serverProperties = {
  targetListFilesLocation: "${targetlist.url}",
  showExtTap: "${show.ext.tap}",
  showEva: "${show.eva}",
- showMissingTranslationBox: "${show.missing.translation.box}"
+ showMissingTranslationBox: "${show.missing.translation.box}",
+ showJwstTimeSeries: "${show.jwst.time.series}"
 };
 
 if(!window.esasky){
